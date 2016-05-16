a528e640b36cda1ba47a0fb903ee23c85d2c6adf
Matt Miller <mwm@fastmail.com>
2016-05-16 12:18:50 +0200

    Update dependencies.

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule Netstrings.Mixfile do
 
   defp deps do
     [
-      {:earmark, ">= 0.0.0", only: :dev},
-      {:ex_doc, "~> 0.11.4", only: :dev},
+      {:earmark, "~> 0.2", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
       {:power_assert, "~> 0.0.8", only: :test},
       {:temp, "~> 0.3.0", only: :test},
     ]
